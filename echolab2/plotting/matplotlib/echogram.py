bb1f77860a485e3da8b0aa7696916663e2bb4865
Rick Towler <rtowler@gmail.com>
2018-01-04 10:52:30 -0800

    changed EK60.EK60 so it stores power instead of indexed power. got get_sv is working after fixing minor bugs with broadcasting issues. fixed but in echogram plotting where the threshold wasn't applied.

--- echolab2/plotting/matplotlib/echogram.py
+++ echolab2/plotting/matplotlib/echogram.py
@@ -17,22 +17,22 @@ class echogram(object):
     def __init__(self, axes, data=None, threshold=None, cmap=None):
 
         self.axes = axes
-        self.threshold = None
+        self.threshold = threshold
 
         #  set the default SIMRAD EK500 color table plus grey for NoData
         self._simrad_color_table = [ (1,1,1),
-                                     (0.6235,0.6235,0.6235),
-                                     (0.3725,0.3725,0.3725),
-                                     (0,0,1),
-                                     (0,0,0.5),
-                                     (0,0.7490,0),
-                                     (0,0.5,0),
-                                     (1,1,0),
-                                     (1,0.5,0),
-                                     (1,0,0.7490),
-                                     (1,0,0),
-                                     (0.6509,0.3255,0.2353),
-                                     (0.4705,0.2353,0.1568)]
+                                              (0.6235,0.6235,0.6235),
+                                              (0.3725,0.3725,0.3725),
+                                              (0,0,1),
+                                              (0,0,0.5),
+                                              (0,0.7490,0),
+                                              (0,0.5,0),
+                                              (1,1,0),
+                                              (1,0.5,0),
+                                              (1,0,0.7490),
+                                              (1,0,0),
+                                              (0.6509,0.3255,0.2353),
+                                              (0.4705,0.2353,0.1568)]
         self._simrad_cmap = LinearSegmentedColormap.from_list('Simrad',  self._simrad_color_table)
         self._simrad_cmap.set_bad(color='grey')
         self.cmap = self._simrad_cmap
