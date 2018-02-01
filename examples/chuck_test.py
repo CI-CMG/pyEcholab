e3c669a3e257eb32d38745d251d7d19d048e4436
Chuck Anderson <charles.anderson@noaa.gov>
2018-01-31 22:43:20 -0700

    Working example for AlignPings

--- examples/chuck_test.py
+++ examples/chuck_test.py
@@ -9,14 +9,13 @@ it. thanks...Chuck
 import sys
 import cProfile
 import pstats
-import numpy as np
+
 from matplotlib.pyplot import figure, show, subplots_adjust, get_cmap
 
 from echolab2.processing.batch_utils import FileAggregator as fa
 from echolab2.processing.align_pings import AlignPings
 from echolab2.instruments.EK60 import EK60
 from echolab2.plotting.matplotlib.echogram import echogram
-
 
 if sys.version_info[0] == 3:
     from io import StringIO
@@ -55,83 +54,105 @@ subplots_adjust(left=0.075, bottom=.05, right=0.98, top=.93, wspace=None,
 #  to interact with EK60 and  data sources
 ek60 = EK60()
 
-# for method in methods:
-#     print(method)
-
-
 #  use the read_raw method to read in a data file
-# profiler.enable()
-# time.clock()
-# ek60.read_raw(raw_files)
 ek60.read_raw(raw_files, power=None, angles=None, max_sample_count=None,
               start_time=None, end_time=None, start_ping=None, end_ping=None,
               frequencies=None, channel_ids=None,
               time_format_string='%Y-%m-%d %H:%M:%S', incremental=None,
               start_sample=None, end_sample=None)
-# print("read time: " + str(time.clock()))
-# profiler.disable()
-# print_profile_stats(profiler)
+
 
 #  print some basic info about our object. As you can see, 10 channels are
 # reported. Each file has 5 channels and they in fact are physically the same
 #  hardware. The reason there are 10 channels reported is that their
 # transceiver number in the ER60 software changed.
+# print(ek60)
 
 
-#  now get a reference to the RawData object that contains data from the
-# first  38 kHz channel.
-raw_data_18 = ek60.get_rawdata(channel_number=1)
-raw_data_38 = ek60.get_rawdata(channel_number=2)
-raw_data_70 = ek60.get_rawdata(channel_number=3)
-raw_data_120 = ek60.get_rawdata(channel_number=4)
-raw_data_200 = ek60.get_rawdata(channel_number=5)
+#  now get a reference to the RawData object for each channel
+raw_18 = ek60.get_rawdata(channel_number=1)
+raw_38 = ek60.get_rawdata(channel_number=2)
+raw_70 = ek60.get_rawdata(channel_number=3)
+raw_120 = ek60.get_rawdata(channel_number=4)
+raw_200 = ek60.get_rawdata(channel_number=5)
+raw = [raw_18, raw_38, raw_70, raw_120, raw_200]
+
+# time 2015-06-26T06:12:03 is missing in the 38kHz channel. this is ping #
+# 489 in he other channels
+
+# uncomment to test aligning of raw data objects
+print('Before alignment')
+for channel in raw:
+    for ping in range(488, 491):
+        print(ping, channel.ping_time[ping], channel.power[ping][100])
+    print()
+
+# call align pings
+AlignPings(raw, 'pad')
+
+print('\n After align')
+for channel in raw:
+    for ping in range(488, 491):
+        print(ping, channel.ping_time[ping], channel.power[ping][100])
+    print()
 
 
+# get Sv for each channel
+Sv_18 = raw_18.get_sv()
+Sv_38 = raw_38.get_sv()
+Sv_70 = raw_70.get_sv()
+Sv_120 = raw_120.get_sv()
+Sv_200 = raw_200.get_sv()
+Sv = [Sv_18, Sv_38, Sv_70, Sv_120, Sv_200]
+
+
+# time 2015-06-26T06:12:03 is missing in the 38kHz channel. this is ping #
+# 489 in he other channels
+
+# uncomment to test aligning of processed data object.
+# print('Before alignment')
+# for channel in Sv:
+#     for ping in range(488, 491):
+#         print(ping, channel.ping_time[ping], channel.Sv[ping][100])
+#     print()
+#
+# # call align pings
+# AlignPings(Sv, 'pad')
+#
+# print('\n After align')
+# for channel in Sv:
+#     for ping in range(488, 491):
+#         print(ping, channel.ping_time[ping], channel.Sv[ping][100])
+#     print()
+
+# plot Sv values
 threshold = [-70, 0]
-Sv_18 = raw_data_18.get_sv()
+
 ax_18 = fig.add_subplot(5, 1, 1)
 echo_18 = echogram(ax_18, Sv_18, 'Sv', threshold=threshold)
 ax_18.set_title("18kHz Sv data in time order")
 # print(Sv_18)
 
-Sv_38 = raw_data_38.get_sv()
 ax_38 = fig.add_subplot(5, 1, 2)
 echo_38 = echogram(ax_38, Sv_38, 'Sv', threshold=threshold)
 ax_38.set_title("38kHz Sv data in time order")
 # print(Sv_38)
 
-Sv_70 = raw_data_70.get_sv()
 ax_70 = fig.add_subplot(5, 1, 3)
 echo_70 = echogram(ax_70, Sv_70, 'Sv', threshold=threshold)
 ax_70.set_title("70kHz Sv data in time order")
 # print(Sv_70)
 
-Sv_120 = raw_data_120.get_sv()
 ax_120 = fig.add_subplot(5, 1, 4)
 echo_120 = echogram(ax_120, Sv_120, 'Sv', threshold=threshold)
 ax_120.set_title("120kHz Sv data in time order")
 # print(Sv_120)
 
-Sv_200 = raw_data_200.get_sv()
 ax_200 = fig.add_subplot(5, 1, 5)
 echo_200 = echogram(ax_200, Sv_200, 'Sv', threshold=threshold)
 ax_200.set_title("200kHz Sv data in time order")
 # print(Sv_200)
 
-print(Sv_18.ping_time[488], Sv_18.ping_time[489], Sv_18.ping_time[490])
-print(Sv_38.ping_time[488], Sv_38.ping_time[489], Sv_38.ping_time[490])
-
-AlignPings([Sv_18, Sv_38, Sv_70, Sv_120, Sv_200], 'pad')
-
-print(Sv_18.ping_time[488], Sv_18.ping_time[489], Sv_18.ping_time[490])
-print(Sv_38.ping_time[488], Sv_38.ping_time[489], Sv_38.ping_time[490], '\n')
-
-
-
-
 #  show our figure
-# show()
+show()
 
-
-
-pass
