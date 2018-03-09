1c339ce246fc996a8bbf65aee361dad1d6cb4c3e
Chuck Anderson <charles.anderson@noaa.gov>
2018-03-09 10:24:57 -0700

    Modified Align pings to create it's own padding object using .empty_like and setting ping times to appropriate values for missing pings.

--- echolab2/processing/align_pings.py
+++ echolab2/processing/align_pings.py
@@ -1,12 +1,6 @@
 # coding=utf-8
-
 """
-Class that takes a list of either raw data objects or processed dat objects 
-and aligns data by ping time. The self.longest and self.shortest of the data objects (
-number of pings) is used to either pad short objects with NaN  value "pings" 
-or delete long objects to length of short object by deleting pings not found 
-in short object.
-
+Chuck Anderson NCEI
 """
 import numpy as np
 
@@ -14,16 +8,19 @@ import numpy as np
 class AlignPings(object):
     def __init__(self, channels, mode='pad'):
         """
+        Aligns pings across data objects passed into the class
+
         Class runs on initialization and passing of list of channels to
         align. Self.longest and self.shortest give access to the channel with
         the longest and shortest ping count. Self.missing is an array of
         arrays of ping missing in shorter objects. Self.extras is an array or
         arrays of pings on longer objects not found in shortest object
 
-        :param channels: list of data objects. these must by channels from 
-        the same reader instance
-        :param mode: either 'pad' for align by padding or 'delete' for align by 
-        removing pings
+        Args:
+            channels (list): List of sample data objects. These must by
+            channels from the same reader instance.
+            mode (str): Either 'pad' for align by padding or 'delete' for
+                align by removing pings.
         """
 
         # Get list of ping_time sizes
@@ -54,6 +51,9 @@ class AlignPings(object):
     @staticmethod
     def _find_missing(channels, longest):
         """
+        Find pings in channels that are missing relative to teh channel with
+        the most pings.
+
         Based on ping times, for each channel find the pings that are missing
         relative to the longest channel. Channels with matching ping time
         arrays will return an empty array for that channel
@@ -61,6 +61,14 @@ class AlignPings(object):
         :param channels: list of sample data objects=, one for each channel
         :param longest: sample data object of chanel with most pings
         :return: array of arrays containing missing pings
+
+        Args:
+            channels (list):  List of sample data objects that was passed
+                into class.
+            longest (int): Index in channels for channel with the most pings.
+
+        Returns: A list of Numpy arrays containing missing pings.
+
         """
         missing = []
         for channel in channels:
@@ -74,13 +82,21 @@ class AlignPings(object):
     @staticmethod
     def _find_extra(channels, shortest):
         """
+        Find pings in channels that are not found in the channel with the
+        fewest pings.
+
         Based on ping times, for each channel find the pings that are in channel
         but not in the shortest channel. Channels with matching ping time
         arrays will return an empty array for that channel
 
-        :param channels: list of sample data objects=, one for each channel
-        :param shortest: sample data object of chanel with fewest pings
-        :return: array of arrays containing extra pings
+        Args:
+            channels (list):  List of sample data objects that was passed
+                into class.
+            shortest (int): Index in channels for channel with the fewest
+                pings.
+
+        Returns:  A list of Numpy arrays containing extra pings.
+
         """
         extras = []
         for channel in channels:
@@ -95,34 +111,39 @@ class AlignPings(object):
     @staticmethod
     def _delete_extras(channels, extras):
         """
+        Delete extra pings from long channels.
+
         Iterate through list of channels and use channel's (sample data
         object) delete method to delete extra pings from long channels.
 
-        :param channels: list of sample data objects=, one for each channel
-        :param extras: array of arrays containing extra pings to be deleted
-        from channels
-        :return: None
-        """
+        Args:
+            channels (list):  List of sample data objects that was passed
+                into class.
+            extras (list): A list of Numpy arrays containing extra pings.
 
+        """
         for index, channel in enumerate(channels):
             channel.delete(index_array=extras[index])
 
-    @staticmethod
-    def _pad_pings(channels, missing):
+    def _pad_pings(self, channels, missing):
         """
+        Insert empty pings into short channels.
+
         Iterate through list of channels. If channel is short and needs to be
         padded. Create a 1-ping long object to use as a pad and use
         channel's insert method to insert pad.
 
-        :param channels: list of sample data objects=, one for each channel
-        :param missing: array of arrays containing missing pings to be padded
-        into shorter channels
-        :return: None
+        Args:
+            channels (list): List of sample data objects that was passed
+                into class.
+            missing (list): A list of Numpy arrays containing extra pings.
         """
-
         for index, channel in enumerate(channels):
             if len(missing[index]) > 0:
-                channel.insert(None, index_array=missing[index])
+                times = channels[self.longest].ping_time[missing[index]]
+                fill = channels[index].empty_like(times.size)
+                fill.ping_time[0:] = times
+                channel.insert(fill, index_array=missing[index])
 
 
 
