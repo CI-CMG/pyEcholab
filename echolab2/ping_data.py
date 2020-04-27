Almost there. Added computation of effective pulse duration. Need to fix bug were effective pulse duration isn't returned when calling raw_data.get_calibration_param.

echolab2/ping_data.py
@@ -926,7 +926,7 @@ class ping_data(object):
         return primary_index[mask]
 
 
-    def _get_calibration_param(self, cal_object, param_name, return_indices,
+    def get_calibration_param(self, cal_object, param_name, return_indices,
                                dtype='float32'):
         """Retrieves calibration parameter values.
 
@@ -969,46 +969,63 @@ class ping_data(object):
             A numpy array, param_data, with calibration parameter values.
         """
 
-        param = getattr(cal_object, param_name)
+        # If we're not given specific indices, grab everything.
+        if return_indices is None:
+            return_indices = np.arange(self.ping_time.shape[0])
+        # Ensure that return_indices is a numpy array
+        elif type(return_indices) is not np.ndarray:
+            return_indices = np.array(return_indices, ndmin=1)
 
-        if not param is None:
+        # Check if the provided calibration object has the requested attribute
+        if hasattr(cal_object, param_name):
 
-            # Check if the input param is a numpy array.
-            if isinstance(param, np.ndarray):
-                # Check if it is a single value array.
-                if param.shape[0] == 1:
-                    param_data = np.empty((return_indices.shape[0]),
-                                          dtype=dtype)
+            # Yes. Get a reference to it
+            param = getattr(cal_object, param_name)
+
+            # Check if it contains data
+            if not param is None:
+
+                # Check if the input param is a numpy array.
+                if isinstance(param, np.ndarray):
+                    # Check if it is a single value array.
+                    if param.shape[0] == 1:
+                        param_data = np.empty((return_indices.shape[0]), dtype=dtype)
+                        param_data.fill(param)
+                    # Check if it is an array the same length as contained in the
+                    # raw data.
+                    elif param.shape[0] == self.ping_time.shape[0]:
+                        # Calibration parameters provided as full length
+                        # array.  Get the selection subset.
+                        param_data = param[return_indices]
+                    # Check if it is an array the same length as return_indices.
+                    elif param.shape[0] == return_indices.shape[0]:
+                        # Calibration parameters provided as a subset, so no need
+                        # to index with return_indices.
+                        param_data = param
+                    else:
+                        # It is an array that is the wrong shape.
+                        raise ValueError("The calibration parameter array " +
+                                param_name + " is the wrong length.")
+                # It is not an array.  Check if it is a scalar int or float.
+                elif type(param) in [int, float, np.int32, np.int64, np.float32, np.float64]:
+                    param_data = np.empty((return_indices.shape[0]), dtype=dtype)
                     param_data.fill(param)
-                # Check if it is an array the same length as contained in the
-                # raw data.
-                elif param.shape[0] == self.ping_time.shape[0]:
-                    # Calibration parameters provided as full length
-                    # array.  Get the selection subset.
-                    param_data = param[return_indices]
-                # Check if it is an array the same length as return_indices.
-                elif param.shape[0] == return_indices.shape[0]:
-                    # Calibration parameters provided as a subset, so no need
-                    # to index with return_indices.
-                    param_data = param
+                elif type(param) in [str, object, dict, list]:
+                    param_data = np.empty((return_indices.shape[0]), dtype='object')
+                    param_data.fill(param)
                 else:
-                    # It is an array that is the wrong shape.
-                    raise ValueError("The calibration parameter array " +
-                                     param_name + " is the wrong length.")
-            # It is not an array.  Check if it is a scalar int or float.
-            elif type(param) in [int, float, np.float32, np.float64]:
-                    param_data = np.empty((return_indices.shape[0]),
-                                              dtype=dtype)
-                    param_data.fill(param)
+                    # Invalid type provided.
+                    raise ValueError("The calibration parameter " + param_name +
+                            " must be an ndarray or scalar value.")
             else:
-                # Invalid type provided.
-                raise ValueError("The calibration parameter " + param_name +
-                        " must be an ndarray or scalar float.")
+                # Parameter is not provided in the calibration object, extract it
+                # from the raw data.
+                param_data = cal_object.get_attribute_from_raw(self, param_name,
+                        return_indices=return_indices)
+
         else:
-            # Parameter is not provided in the calibration object, extract it
-            # from the raw data.
-            param_data = cal_object.get_attribute_from_raw(self, param_name,
-                    return_indices=return_indices)
+            # The requested parameter is not availailable
+            param_data = None
 
         return param_data
 

test/ek80_compare.py
@@ -24,8 +24,8 @@ from matplotlib.pyplot import figure, show
 
 #  define the paths to the reference data files
 raw_filename = 'C:/EK80 Test Data/EK80/CW/reduced/DY2000_EK80_Cal-D20200126-T061004.raw'
-ev_filename = 'C:/EK80 Test Data/EK80/CW/reduced/DY2000_EK80_Cal-D20200126-T061004-38kHz-Sv.mat'
-
+ev_Sv_filename = 'C:/EK80 Test Data/EK80/CW/reduced/DY2000_EK80_Cal-D20200126-T061004-38kHz-Sv.mat'
+ev_Ts_filename = 'C:/EK80 Test Data/EK80/CW/reduced/DY2000_EK80_Cal-D20200126-T061004-38kHz-Ts.ts.csv'
 
 #raw_filename = 'C:/EK80 Test Data/EK60/DY1803-D20180312-T212915.raw'
 #ev_filename = 'C:/EK80 Test Data/EK60/DY1803-D20180312-T212915-38kHz-Sv.mat'
@@ -43,38 +43,60 @@ raw_data = raw_38[0]
 
 #  get a cal object populated with params from the raw_data object
 calibration = raw_data.get_calibration()
-#calibration.pulse_duration = 0.0008306
-
 
 #  convert to Sv
 ek80_Sv = raw_data.get_Sv(calibration=calibration)
+
+#  and convert to Ts
+ek80_Ts = raw_data.get_Sp(calibration=calibration)
 
 fig = figure()
 eg = echogram.Echogram(fig, ek80_Sv, threshold=[-70,-34])
 eg.axes.set_title("Echolab2 Sv")
 
+
+fig5 = figure()
+eg5 = echogram.Echogram(fig5, ek80_Ts, threshold=[-70,-34])
+eg5.axes.set_title("Echolab2 Ts")
+
+
+
+
 #  read in the echoview data - we can read .mat files exported
 #  from EV 7+ directly into a processed_data object
-ev_data = processed_data.read_ev_mat('EV Sv 38 kHz', 38000., ev_filename)
+ev_Sv_data = processed_data.read_ev_mat('EV Sv 38 kHz', 38000., ev_Sv_filename)
+
+#  we can also read in .csv formatted files
+ev_Ts_data = processed_data.read_ev_csv('EV Ts 38 kHz', 38000., ev_Ts_filename)
 
 fig2 = figure()
-eg2 = echogram.Echogram(fig2, ev_data, threshold=[-70,-34])
+eg2 = echogram.Echogram(fig2, ev_Sv_data, threshold=[-70,-34])
 eg2.axes.set_title("Echoview Sv")
 
+fig6 = figure()
+eg6 = echogram.Echogram(fig6, ev_Ts_data, threshold=[-70,-34])
+eg6.axes.set_title("Echoview Sv")
+
 #  compute the difference of these data
-diff = ek80_Sv - ev_data
+diff = ek80_Sv - ev_Sv_data
 fig3 = figure()
 eg3 = echogram.Echogram(fig3, diff, threshold=[-1,1])
-eg3.axes.set_title("Difference")
+eg3.axes.set_title("Sv Difference")
+
+#  compute the difference of these data
+diff = ek80_Ts - ev_Ts_data
+fig4 = figure()
+eg4 = echogram.Echogram(fig4, diff, threshold=[-1,1])
+eg4.axes.set_title("Ts Difference")
 
 # Show our figures.
 show()
 
 
 
-ev_ping = ev_data['Data_values'][10,:]
-fig = plt.plot(np.arange(ev_ping.shape[0]), ev_ping, label='Echoview', color='blue', linewidth=2)
-fig = plt.plot(np.arange(echolab2_Sv.data[10,1:].shape[0]), echolab2_Sv.data[10,1:], label='pyEcholab2', color='red')
-plt.legend()
-plt.show(block=True)
+#ev_ping = ev_data['Data_values'][10,:]
+#fig = plt.plot(np.arange(ev_ping.shape[0]), ev_ping, label='Echoview', color='blue', linewidth=2)
+#fig = plt.plot(np.arange(echolab2_Sv.data[10,1:].shape[0]), echolab2_Sv.data[10,1:], label='pyEcholab2', color='red')
+#plt.legend()
+#plt.show(block=True)
 